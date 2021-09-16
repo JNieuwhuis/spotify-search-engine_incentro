7bf6c173a68429cc674a81d79d7296d26c0c39e7
JNieuwhuis <J.Nieuwhuis@hotmail.com>
2021-09-16 14:52:41 +0200

    Basic SearchBar

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SearchBar />
+        <h1>Incentro Spotify Search Engine</h1>
       </header>
+      <main className="App-main">
+        <SearchBar />
+      </main>
     </div>
   );
 }
--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
+import "../Styles/SearchBar.css";
 
 const SearchBar = () => {
-    return (
-        <p>
-            Hello World
-        </p>
-    );
-}
+  const [input, setInput] = useState<string>("");
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleSearch = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("input", input);
+  };
+
+  return (
+    <div className="searchBox">
+      <button className="searchButton" onClick={handleSearch}>
+        Search!
+      </button>
+      <input
+        className="inputBar"
+        placeholder="Search Spotify..."
+        onChange={handleInput}
+      />
+    </div>
+  );
+};
 
 export default SearchBar;
